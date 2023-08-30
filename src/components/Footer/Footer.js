618859c7ddf267cf185b2f4320b34cb667db20a2
AnthonyB0511 <137073922+AnthonyB0511@users.noreply.github.com>
2023-08-30 17:57:51 +0200

    :sparkles: :feat burgerMenu

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,12 @@
 import styles from "./Footer.module.scss"
+import { Contact } from "./Contact"
 export default function Footer(){
     return (
         <footer className={`${styles.footer}`}>
             <div className={`d-flex justify-content-center align-items-center flex-column`}>
                 <p>© 2023 Les Dés Gommés - <span className={`${styles.rights}`}>Tous droits réservés</span></p>
             </div>
-            <div className={`d-flex align-items-center justify-content-center`}>
-               <i class="fa-solid fa-envelope"></i>
-                <a href="mailto:lesdesgommes@gmail.com"> lesdesgommes@gmail.com</a>
-                <p> | </p> 
-                <a href=""><i class="fa-brands fa-facebook"></i></a> 
-            </div>              
+            <Contact />              
             <div className={`${styles.mention} d-flex justify-content-center align-items-center`}>
                 <a href="">Mentions légales</a>
                 <p className="space"> | </p>
--- src/components/Header/Burger.js
+++ src/components/Header/Burger.js
@@ -1,8 +1,36 @@
-import styles from "./Burger.module.scss"
-export default function(){
-    return(
-        <div className={`${styles.icon}`}>
-            <i className="fa-solid fa-bars"></i>
+import styles from "./Burger.module.scss";
+import { Nav } from "./Nav";
+import { useState } from "react";
+export default function Burger(){
+    const [open,setOpen] = useState(false);
+
+    const HamburgerIcon = <div 
+                            className={`${styles.icon}`}
+                            onClick={() => setOpen(!open)}>
+                            <i className="fa-solid fa-bars"></i>
+                        </div>
+    const CloseIcon = <>
+                        <Nav />
+                        <div 
+                        className={`${styles.icon}`}
+                        onClick={() => setOpen(!open)}>
+                           <i className="fa-solid fa-circle-xmark"></i>
+                        </div>
+                    </>
+
+    return( 
+        <div>
+            {open ? CloseIcon : HamburgerIcon}
         </div>
+        
+        // {open && 
+        // <div>
+        //     <Nav />
+        //     <CloseBurger 
+        //     onClick={() => setOpen(!open)}/>  
+        // </div>
+        // }
+        // </>
+        
     )
 }
