f6874d622dc504b1ff15cf73ee82f20e8fd6bc5d
AnthonyB0511 <137073922+AnthonyB0511@users.noreply.github.com>
2023-09-11 22:03:30 +0200

    :iphone: :feat responsive présentation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,24 @@
-import styles from "./Card.module.scss"
-import { Button } from "./Button"
+import styles from "./Card.module.scss";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
 
-export function Card ({actu}) {
-    return(
+export function Card({ actu }) {
+    return (
         <div className={`${styles.card} m20`}>
             <div className={`${styles.img}`}>
                 <img src={actu.image} alt="" />
             </div>
             <div className={`${styles.content}`}>
                 <p>{actu.text}</p>
-                <Button 
-                needButton={actu.needButton}
-                txtButton="Cliquer ICI"/>
+                <Link to="/presentation" >
+                    <Button
+                        needButton={actu.needButton}
+                        txtButton="Cliquer ICI" />
+                </Link>
+
             </div>
         </div>
-       
-        
+
+
     )
 }
